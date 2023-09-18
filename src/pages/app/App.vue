16534545118994882c26395231766b2c4f70c416
uxDimon <bobyorrr@gmail.com>
2023-09-18 14:27:41 +0300

    + style

--- src/pages/app/App.vue
+++ src/pages/app/App.vue
@@ -4,11 +4,59 @@ import { DRINK_LIST } from "@/pages/app/constsApp";
 </script>
 
 <template>
-	<nav>
-		<RouterLink v-for="index in DRINK_LIST" :key="index" :to="'/' + index">{{ index }}</RouterLink>
+	<nav class="nav">
+		<RouterLink v-for="index in DRINK_LIST" :key="index" :to="'/' + index" class="nav-link">{{ index }}</RouterLink>
 	</nav>
 
 	<RouterView />
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+div#app {
+	display: grid;
+	grid-template-columns: 200px 1fr;
+	grid-column-gap: 16px;
+
+	width: 100%;
+	max-width: var(--block-max-width);
+	margin: 0 auto;
+	padding: 16px;
+}
+
+@media screen and (max-width: 820px) {
+	div#app {
+		grid-template-columns: 1fr;
+		gap: 32px;
+	}
+}
+</style>
+
+<style scoped lang="scss">
+.nav {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 6px;
+}
+
+.nav-link {
+	width: 100%;
+	padding: 4px;
+
+	&.router-link-active {
+		background: hsl(0, 0%, 30%);
+	}
+}
+
+@media screen and (max-width: 820px) {
+	.nav {
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 6px 12px;
+	}
+
+	.nav-link {
+		width: auto;
+	}
+}
+</style>
--- src/pages/app/modules/app-drink/AppDrink.vue
+++ src/pages/app/modules/app-drink/AppDrink.vue
@@ -48,12 +48,10 @@ const drinkItem = computed(() => store.drink[routeName.value]);
 </script>
 
 <template>
-	<div v-if="drinkItem" class="drink">
-		<img v-if="drinkItem.strDrinkThumb" :src="drinkItem.strDrinkThumb" :alt="drinkItem.strDrink" />
-
+	<main v-if="drinkItem" class="drink">
 		<div class="info">
 			<h1 class="title">{{ drinkItem.strDrink }}</h1>
-			<ul>
+			<ul class="list">
 				<li>
 					{{ drinkItem.strCategory }}
 				</li>
@@ -65,10 +63,10 @@ const drinkItem = computed(() => store.drink[routeName.value]);
 				</li>
 			</ul>
 
-			<h3>Instructions:</h3>
+			<h3 class="subtitle">Instructions:</h3>
 			<p>{{ drinkItem.strInstructions }}</p>
 
-			<h3>List of ingredients:</h3>
+			<h3 class="subtitle">List of ingredients:</h3>
 			<table>
 				<tr v-for="item in drinkItem.ingredients" :key="item.name">
 					<td>{{ item.name }}</td>
@@ -76,7 +74,86 @@ const drinkItem = computed(() => store.drink[routeName.value]);
 				</tr>
 			</table>
 		</div>
+
+		<img
+			v-if="drinkItem.strDrinkThumb"
+			:src="drinkItem.strDrinkThumb"
+			:alt="drinkItem.strDrink"
+			loading="lazy"
+			class="img"
+			width="300"
+			height="300"
+		/>
+	</main>
+
+	<div v-else class="spinner">
+		<svg width="44" height="44" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+				opacity=".25"
+				fill="#fff"
+			/>
+			<path
+				d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+				class="spinner_ajPY"
+				fill="#fff"
+			/>
+		</svg>
 	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style>
+.spinner_ajPY {
+	transform-origin: center;
+	animation: spinner_AtaB 0.75s infinite linear;
+}
+@keyframes spinner_AtaB {
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
+
+<style scoped lang="scss">
+.drink {
+	display: grid;
+	grid-template-columns: 1fr 300px;
+	grid-column-gap: 16px;
+}
+
+.title {
+	margin: 0 0 12px;
+}
+
+.subtitle {
+	margin: 16px 0 8px;
+}
+
+.list {
+	margin-bottom: 20px;
+}
+
+.img {
+	width: 100%;
+	height: auto;
+	max-height: 400px;
+	object-fit: cover;
+}
+
+.spinner {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+@media screen and (max-width: 640px) {
+	.drink {
+		grid-template-columns: 1fr;
+		gap: 32px;
+	}
+
+	.img {
+		grid-row-start: 1;
+	}
+}
+</style>
